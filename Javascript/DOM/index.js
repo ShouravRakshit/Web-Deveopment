7a0e0c58069e1b0739c2608654808b19a54fdf6d
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-07-24 14:59:26 -0600

    Created a game called Dicee.

---
+++ Javascript/DOM/index.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").style.color = "blue";
+document.querySelector(".list a").style.color = "yellow";
+document.querySelector("button").style.backgroundColor = "black";
+document.querySelector("button").style.color = "white";
+document.querySelector("button").style.fontSize = "20px";
---
+++ Javascript/Dicee Game/script.js
@@ -0,0 +1,21 @@
+function getRandomNumber() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function checkWinner(player1, player2) {
+    if (player1 === player2) {
+        document.querySelector("h1").textContent = "Draw";
+    } else if(player1 > player2){
+        document.querySelector("h1").textContent = "PLayer 1 Wins";
+    }else{
+        document.querySelector("h1").textContent = "PLayer 2 Wins";
+    }
+}
+
+const randomNumber1 = getRandomNumber();
+const randomNumber2 = getRandomNumber();
+
+checkWinner(randomNumber1, randomNumber2);
+document.querySelector(".img1").src = `./images/dice${randomNumber1}.png`;
+document.querySelector(".img2").src = `./images/dice${randomNumber2}.png`;
+
