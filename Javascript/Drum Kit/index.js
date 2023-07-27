1c7e3f3b2449a069bdb62ef4b90cc4fe4f6d26f0
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-07-26 19:50:46 -0600

    Update index.js

--- Javascript/Drum Kit/index.js
+++ Javascript/Drum Kit/index.js
@@ -2,20 +2,66 @@ const element = document.querySelectorAll(".drum");
 console.log(element)
 
 
-var myKey;
+function makeSound(myButton) {
+  
+  switch(myButton) {
+    case "w":
+      var audio = new Audio('./sounds/crash.mp3');
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio('./sounds/kick-bass.mp3');
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio('./sounds/snare.mp3');
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio('./sounds/tom-4.mp3');
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio('./sounds/tom-1.mp3');
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio('./sounds/tom-2.mp3');
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio('./sounds/tom-3.mp3');
+      audio.play();
+      break;
+
+
+    default:
+      // code block
+  }
+
+}
+
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    
+    setTimeout(function() {
+      activeButton.classList.remove("pressed");
+
+    }, 200)
+}
+
 
 document.addEventListener("keypress", function(event) {
-  // This function will be called whenever a key is pressed
-  myKey = event.key;
-  // Now I can call any other functions or do any other tasks that rely on myKey here
-  
-  doSomethingWithMyKey(myKey);
+      
+    makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
-function doSomethingWithMyKey(myKey) {
-  // You can use myKey here or perform any other operations with it
-  return myKey;
-}
+
 
 
 for (let index = 0; index < element.length; index++) {
@@ -23,43 +69,12 @@ for (let index = 0; index < element.length; index++) {
       
         var myButton =  this.textContent;
         
-        switch(myButton) {
-          case "w":
-            var audio = new Audio('./sounds/crash.mp3');
-            audio.play();
-            break;
-          case "a":
-            var audio = new Audio('./sounds/kick-bass.mp3');
-            audio.play();
-            break;
-          case "s":
-            var audio = new Audio('./sounds/snare.mp3');
-            audio.play();
-            break;
-          case "d":
-            var audio = new Audio('./sounds/tom-4.mp3');
-            audio.play();
-            break;
-          case "j":
-            var audio = new Audio('./sounds/tom-1.mp3');
-            audio.play();
-            break;
-          case "k":
-            var audio = new Audio('./sounds/tom-2.mp3');
-            audio.play();
-            break;
-          case "l":
-            var audio = new Audio('./sounds/tom-3.mp3');
-            audio.play();
-            break;
-
-
-          default:
-            // code block
-        }
-     
+        makeSound(myButton);
+        buttonAnimation(myButton);
       });
     
 }
+
+
 
 
