f3997c20d537cc0f7fbc06a6640e7871909cae6d
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-08-07 23:35:59 -0600

    Update teacher.js

--- Node.js/2.2 Native Modules/teacher.js
+++ Node.js/2.2 Native Modules/teacher.js
@@ -1,4 +1,9 @@
-const s1 = require("./student");
-console.log(s1.Hi());
+// const s1 = require("./student");
+// console.log(s1.Hi());
 
-console.log(s1.square(7));
+// console.log(s1.square(7));
+
+const learn_os = require("os");
+
+console.log(learn_os.userInfo());
+console.log(learn_os.homedir());
