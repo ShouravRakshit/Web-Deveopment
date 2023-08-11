7f6e2db1748dc8e21b074fb91c6a30eedbfe2fdc
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-08-11 13:48:22 -0600

    Update index.js

--- Express.js/EJS Tags/index.js
+++ Express.js/EJS Tags/index.js
@@ -1,1 +1,20 @@
 
+import express from "express";
+const app = express();
+const port = 3000;
+
+const data = {
+  title: "EJS Tags",
+  seconds: new Date().getSeconds(),
+  items: ["apple", "banana", "cherry"],
+  htmlContent: "<strong>This is some strong text</strong>",
+};
+
+app.get("/", (req, res) => {
+ 
+  res.render("index.ejs", {new_data: data});
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
